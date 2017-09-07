f3c5ae4a9d3574b7401d8447fec1f1a543ef77a7
bastrakov_s <sergey.bastrakov@gmail.com>
2017-09-07 17:44:24 +0300

    Add simple classes to represent 1d, 2d and 3d arrays, and tests.

---
+++ include/pica/utility/Array.h
@@ -0,0 +1,160 @@
+#ifndef PICA_ARRAY_H
+#define PICA_ARRAY_H
+
+
+#include "pica/utility/Assert.h"
+#include "pica/math/Vectors.h"
+
+#include <vector>
+
+
+namespace pica {
+
+
+// A simple class for 1d array (vector) of type T
+template<typename T>
+class Array1d {
+public:
+    typedef T ValueType;
+    typedef int IndexType;
+
+    // Create an empty vector
+    Array1d() {}
+
+    // Create an array of the given size with the given value of elements
+    Array1d(IndexType size, ValueType value = 0) :
+        data(size, value) {}
+
+    // Get size of the vector
+    IndexType getSize() const { return static_cast<IndexType>(data.size()); }
+
+    // Access the value by index without checking the index
+    ValueType& operator()(IndexType index)
+    {
+        PICA_ASSERT((index >= 0) && (index < getSize()));
+        return data[index];
+    }
+
+    const ValueType& operator()(IndexType index) const
+    {
+        PICA_ASSERT((index >= 0) && (index < getSize()));
+        return data[index];
+    }
+
+private:
+    std::vector<T> data;
+};
+
+
+// A simple class for 2d array (vector) of type T
+template<typename T>
+class Array2d {
+public:
+    typedef T ValueType;
+    typedef Vector2<int> IndexType;
+
+    // Create an empty matrix
+    Array2d() :
+        size(0, 0) {}
+
+    // Create a matrix of the given size with the given value of elements
+    Array2d(int nRows, int nCols, ValueType value = 0) :
+        Array2d(IndexType(nRows, nCols), value) {}
+
+    Array2d(IndexType size, ValueType value = 0) :
+        size(size), data(size.volume(), value) {}
+
+    // Get number of rows, columns and total number of elements
+    IndexType getSize() const { return size; }
+    int getNumRows() const { return size.x; }
+    int getNumCols() const { return size.z; }
+    int getNumElements() const { return (int)data.size(); }
+
+    // Access the value by index without checking the index
+    ValueType& operator()(int i, int j)
+    {
+        PICA_ASSERT((i >= 0) && (i < size.x) && (j >= 0) && (j < size.y));
+        return data[i * size.y + j];
+    }
+
+    const ValueType& operator()(int i, int j) const
+    {
+        PICA_ASSERT((i >= 0) && (i < size.x) && (j >= 0) && (j < size.y));
+        return data[i * size.y + j];
+    }
+
+    ValueType& operator()(IndexType index)
+    {
+        PICA_ASSERT((index.x >= 0) && (index.x < size.x) && (index.y >= 0) && (index.y < size.y));
+        return data[index.x * size.y + index.y];
+    }
+
+    const ValueType& operator()(IndexType index) const
+    {
+        PICA_ASSERT((index.x >= 0) && (index.x < size.x) && (index.y >= 0) && (index.y < size.y));
+        return data[index.x * size.y + index.y];
+    }
+
+private:
+    IndexType size;
+    std::vector<T> data;
+};
+
+
+// A simple class for 3d array (vector) of type T
+template<typename T>
+class Array3d  {
+public:
+    typedef T ValueType;
+    typedef Vector3<int> IndexType;
+
+    // Create an empty 3d array
+    Array3d() :
+        size(0, 0, 0) {}
+
+    // Create a matrix of the given size with the given value of elements
+    Array3d(int n1, int n2, int n3, T value = 0) :
+        Array3d(IndexType(n1, n2, n3), value) {}
+
+    Array3d(IndexType size, T value = 0) :
+        size(size), data(size.x * size.y * size.z, value) {}
+
+    // Get size and total number of elements
+    IndexType getSize() const { return size; }
+    int getNumElements() const { return data.size(); }
+
+    // Access the value by index without checking the index
+    ValueType& operator()(int i, int j, int k)
+    {
+        PICA_ASSERT((i >= 0) && (i < size.x) && (j >= 0) && (j < size.y) && (k >= 0) && (k < size.z));
+        return data[(i * size.y + j) * size.z + k];
+    }
+
+    const ValueType& operator()(int i, int j, int k) const
+    {
+        PICA_ASSERT((i >= 0) && (i < size.x) && (j >= 0) && (j < size.y) && (k >= 0) && (k < size.z));
+        return data[(i * size.y + j) * size.z + k];
+    }
+
+    ValueType& operator()(IndexType index)
+    {
+        PICA_ASSERT((index.x >= 0) && (index.x < size.x) && (index.y >= 0) && (index.y < size.y) && (index.z >= 0) && (index.z < size.z));
+        return data[(index.x * size.y + index.y) * size.z + index.z];
+    }
+
+    const ValueType& operator()(IndexType index) const
+    {
+        PICA_ASSERT((index.x >= 0) && (index.x < size.x) && (index.y >= 0) && (index.y < size.y) && (index.z >= 0) && (index.z < size.z));
+        return raw[(index.x * size.y + index.y) * size.z + index.z];
+    }
+
+private:
+    IndexType size;
+    std::vector<T> data;
+};
+    
+
+} // namespace pica
+
+
+#endif
---
+++ test/src/utility/ArrayTests.cpp
@@ -0,0 +1,160 @@
+#include "TestingUtility.h"
+
+#include "pica/utility/Array.h"
+
+#include <typeinfo>
+
+using namespace pica;
+
+
+TEST(Array1dTest, ValueType)
+{
+    ASSERT_TRUE(typeid(Array1d<float>::ValueType) == typeid(float));
+}
+
+TEST(Array1dTest, DefaultConstructor)
+{
+    Array1d<double> a;
+    ASSERT_EQ(0, a.getSize());
+}
+
+TEST(Array1dTest, Constructor)
+{
+    Array1d<int> a(5);
+    ASSERT_EQ(5, a.getSize());
+    for (int i = 0; i < a.getSize(); i++)
+        ASSERT_EQ(0, a(i));
+    Array1d<float> b(9, -3.0f);
+    ASSERT_EQ(9, b.getSize());
+    for (int i = 0; i < b.getSize(); i++)
+        ASSERT_EQ(-3.0f, b(i));
+}
+
+TEST(Array1dTest, GetSize)
+{
+    const Array1d<double> a(6);
+    ASSERT_EQ(6, a.getSize()); 
+}
+
+TEST(Array1dTest, Access)
+{
+    Array1d<int> a(4, -1);
+    for (int i = 0; i < 4; i++)
+        a(i) = i;
+    for (int i = 0; i < 4; i++)
+        ASSERT_EQ(i, a(i));
+}
+
+TEST(Array1dTest, AccessConst)
+{
+    const Array1d<int> a(5, 2);
+    for (int i = 0; i < 5; i++)
+        ASSERT_EQ(2, a(i));
+}
+
+
+TEST(Array2dTest, ValueType)
+{
+    ASSERT_TRUE(typeid(Array2d<int>::ValueType) == typeid(int));
+}
+
+TEST(Array2dTest, DefaultConstructor)
+{
+    Array2d<float> a;
+    ASSERT_TRUE(Array2d<double>::IndexType(0, 0) == a.getSize());
+}
+
+TEST(Array2dTest, Constructor)
+{
+    Array2d<double> a(4, 3);
+    ASSERT_TRUE(Array2d<double>::IndexType(4, 3) == a.getSize());
+    for (int i = 0; i < a.getSize().x; i++)
+    for (int j = 0; j < a.getSize().y; j++)
+        ASSERT_EQ(0, a(i, j));
+    Array2d<int> b(Array2d<int>::IndexType(5, 2), 3);
+    ASSERT_TRUE(Array2d<double>::IndexType(5, 2) == b.getSize());
+    for (int i = 0; i < b.getSize().x; i++)
+    for (int j = 0; j < b.getSize().y; j++)
+        ASSERT_EQ(3, b(i, j));
+}
+
+TEST(Array2dTest, GetSize)
+{
+    const Array2d<float> a(7, 4);
+    ASSERT_TRUE(Array2d<float>::IndexType(7, 4) == a.getSize());
+}
+
+TEST(Array2dTest, Access)
+{
+    Array2d<int> a(3, 5, -1);
+    for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 5; j++)
+        a(i, j) = i + j;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 5; j++)
+        ASSERT_EQ(i + j, a(i, j));
+}
+
+TEST(Array2dTest, AccessConst)
+{
+    const Array2d<double> a(7, 3, -1.0);
+    for (int i = 0; i < 7; i++)
+    for (int j = 0; j < 3; j++)
+        ASSERT_EQ(-1.0, a(i, j));
+}
+
+
+TEST(Array3dTest, ValueType)
+{
+    ASSERT_TRUE(typeid(Array3d<double>::ValueType) == typeid(double));
+}
+
+TEST(Array3dTest, DefaultConstructor)
+{
+    Array3d<double> a;
+    ASSERT_TRUE(Array3d<double>::IndexType(0, 0, 0) == a.getSize());
+}
+
+TEST(Array3dTest, Constructor)
+{
+    Array3d<double> a(4, 5, 2);
+    ASSERT_TRUE(Array3d<double>::IndexType(4, 5, 2) == a.getSize());
+    for (int i = 0; i < a.getSize().x; i++)
+    for (int j = 0; j < a.getSize().y; j++)
+    for (int k = 0; k < a.getSize().z; k++)
+        ASSERT_EQ(0, a(i, j, k));
+    Array3d<int> b(Array3d<double>::IndexType(5, 2, 4), 10);
+    ASSERT_TRUE(Array3d<double>::IndexType(5, 2, 4) == b.getSize());
+    for (int i = 0; i < b.getSize().x; i++)
+    for (int j = 0; j < b.getSize().y; j++)
+    for (int k = 0; k < b.getSize().z; k++)
+        ASSERT_EQ(10, b(i, j, k));
+}
+
+TEST(Array3dTest, GetSize)
+{
+    const Array3d<float> a(17, 4, 6);
+    ASSERT_TRUE(Array3d<double>::IndexType(17, 4, 6) == a.getSize());
+}
+
+TEST(Array3dTest, Access)
+{
+    Array3d<int> a(6, 4, 1, -1);
+    for (int i = 0; i < 6; i++)
+    for (int j = 0; j < 4; j++)
+    for (int k = 0; k < 1; k++)
+        a(i, j, k) = i + j + k;
+    for (int i = 0; i < 6; i++)
+    for (int j = 0; j < 4; j++)
+    for (int k = 0; k < 1; k++)
+        ASSERT_EQ(i + j + k, a(i, j, k));
+}
+
+TEST(Array3dTest, AccessConst)
+{
+    const Array3d<double> a(9, 5, 7, 3.5);
+    for (int i = 0; i < 9; i++)
+    for (int j = 0; j < 5; j++)
+    for (int k = 0; k < 7; k++)
+        ASSERT_EQ(3.5, a(i, j, k));
+}
