91f9476c104b197d8ca8508c18b5ce9e9ce37bad
bastrakov_s <sergey.bastrakov@gmail.com>
2017-12-04 18:22:38 +0300

    Add implementation of field interpolation with preloading.

--- include/pica/fieldInterpolation/FieldInterpolatorYeeGrid.h
+++ include/pica/fieldInterpolation/FieldInterpolatorYeeGrid.h
@@ -183,6 +183,106 @@ private:
 };
 
 
+template<typename Real>
+class FieldInterpolatorCICSupercell {
+public:
+
+    typedef YeeGrid<Three, Real> GridType;
+    typedef typename GridType::PositionType PositionType;
+    typedef typename GridType::IndexType IndexType;
+    typedef typename GridType::ValueType ValueType;
+    typedef typename ScalarType<PositionType>::Type ScalarPositionType;
+
+    FieldInterpolatorCICSupercell(const GridType& grid, const PositionType& minPosition,
+        const IndexType& numCellsPerSupercell) :
+        grid(grid),
+        inverseStep(inverse(grid.getStep())),
+        normalizedOrigin((minPosition - grid.getStep()) / grid.getStep()),
+        normalizedOriginStaggered(normalizedOrigin + ones<Three, ScalarPositionType>() * static_cast<ScalarPositionType>(0.5))
+    {
+        IndexType baseIdx = truncate(normalizedOriginStaggered);
+        IndexType size = numCellsPerSupercell + IndexType(2, 2, 2);
+        /// Poor man's check
+        for (int d = 0; d < 3; d++)
+            if (size[d] > maxSize)
+                std::cout << "ERROR: too large supercell size for interpolator\n";
+
+        for (int i = 0; i < size.x; i++)
+        for (int j = 0; j < size.y; j++)
+        for (int k = 0; k < size.z; k++)
+        {
+            IndexType gridxIdx = baseIdx + IndexType(i, j, k);
+            ex[i][j][k] = grid.ex(gridxIdx.x, gridxIdx.y, gridxIdx.z);
+            ey[i][j][k] = grid.ey(gridxIdx.x, gridxIdx.y, gridxIdx.z);
+            ez[i][j][k] = grid.ez(gridxIdx.x, gridxIdx.y, gridxIdx.z);
+            bx[i][j][k] = grid.bx(gridxIdx.x, gridxIdx.y, gridxIdx.z);
+            by[i][j][k] = grid.by(gridxIdx.x, gridxIdx.y, gridxIdx.z);
+            bz[i][j][k] = grid.bz(gridxIdx.x, gridxIdx.y, gridxIdx.z); 
+        }
+    }
+
+    void get(const PositionType position, Vector3<ValueType>& e, Vector3<ValueType>& b)
+    {
+        IndexType indexCollocated, indexStaggered;
+        PositionType coeffCollocated, coeffStaggered;
+        getIndexCoeff(position, indexCollocated, coeffCollocated, indexStaggered, coeffStaggered);
+        e.x = interpolate(ex, IndexType(indexCollocated.x, indexStaggered.y, indexStaggered.z),
+            PositionType(coeffCollocated.x, coeffStaggered.y, coeffStaggered.z));
+        e.y = interpolate(ey, IndexType(indexStaggered.x, indexCollocated.y, indexStaggered.z),
+            PositionType(coeffStaggered.x, coeffCollocated.y, coeffStaggered.z));
+        e.z = interpolate(ey, IndexType(indexStaggered.x, indexStaggered.y, indexCollocated.z),
+            PositionType(coeffStaggered.x, coeffStaggered.y, coeffCollocated.z));
+        b.x = interpolate(bx, IndexType(indexStaggered.x, indexCollocated.y, indexCollocated.z),
+            PositionType(coeffStaggered.x, coeffCollocated.y, coeffCollocated.z));
+        b.y = interpolate(by, IndexType(indexCollocated.x, indexStaggered.y, indexCollocated.z),
+            PositionType(coeffCollocated.x, coeffStaggered.y, coeffCollocated.z));
+        b.z = interpolate(bz, IndexType(indexCollocated.x, indexCollocated.y, indexStaggered.z),
+            PositionType(coeffCollocated.x, coeffCollocated.y, coeffStaggered.z));
+    }
+
+private:
+
+    const GridType& grid;
+    PositionType inverseStep;
+    PositionType normalizedOrigin, normalizedOriginStaggered;
+    static const int maxSize = 6; // corresponds to max supercell size 4
+    ValueType ex[maxSize][maxSize][maxSize];
+    ValueType ey[maxSize][maxSize][maxSize];
+    ValueType ez[maxSize][maxSize][maxSize];
+    ValueType bx[maxSize][maxSize][maxSize];
+    ValueType by[maxSize][maxSize][maxSize];
+    ValueType bz[maxSize][maxSize][maxSize];
+
+    void getIndexCoeff(const PositionType& position, IndexType& indexCollocated, PositionType& coeffCollocated,
+        IndexType& indexStaggered, PositionType& coeffStaggered) const
+    {
+        PositionType normalizedPosition = position * inverseStep;
+        PositionType internalPosition = normalizedPosition - normalizedOrigin;
+        indexCollocated = truncate(internalPosition);
+        coeffCollocated = internalPosition - PositionType(indexCollocated);
+        internalPosition = normalizedPosition - normalizedOriginStaggered;
+        indexStaggered = truncate(internalPosition);
+        coeffStaggered = internalPosition - PositionType(indexStaggered);
+    }
+
+    ValueType interpolate(ValueType component[maxSize][maxSize][maxSize],
+        const IndexType& baseIndex, const PositionType& coeff) const
+    {
+        return
+            component[baseIndex.x][baseIndex.y][baseIndex.z] * (1.0 - coeff.x) * (1.0 - coeff.y) * (1.0 - coeff.z) +
+            component[baseIndex.x][baseIndex.y][baseIndex.z + 1] * (1.0 - coeff.x) * (1.0 - coeff.y) * coeff.z +
+            component[baseIndex.x][baseIndex.y + 1][baseIndex.z] * (1.0 - coeff.x) * coeff.y * (1.0 - coeff.z) +
+            component[baseIndex.x][baseIndex.y + 1][baseIndex.z + 1] * (1.0 - coeff.x) * coeff.y * coeff.z +
+            component[baseIndex.x + 1][baseIndex.y][baseIndex.z] * (1.0 - coeff.x) * (1.0 - coeff.y) * (1.0 - coeff.z) +
+            component[baseIndex.x + 1][baseIndex.y][baseIndex.z + 1] * coeff.x * (1.0 - coeff.y) * coeff.z +
+            component[baseIndex.x + 1][baseIndex.y + 1][baseIndex.z] * coeff.x * coeff.y * (1.0 - coeff.z) +
+            component[baseIndex.x + 1][baseIndex.y + 1][baseIndex.z + 1] * coeff.x * coeff.y * coeff.z;
+    }
+
+};
+
+
+
 } // namespace pica
 
 
