3fa45412318388f35c7f82690c76f3249a64785d
isurmin <i.surmin@gmail.com>
2018-05-28 02:31:19 +0300

    correct code by passing particles to pusher by pointer

--- include/pica/particlePush/BorisPusherBaseline.h
+++ include/pica/particlePush/BorisPusherBaseline.h
@@ -17,19 +17,19 @@ struct BorisPusherBaseline {
     typedef typename ParticleTraits<Particle>::MomentumType MomentumType;
 
     template<class ParticleRef, typename Real>
-    void push(ParticleRef particle, const MomentumType& e, const MomentumType& b, Real dt)
+    void push(ParticleRef* particle, const MomentumType& e, const MomentumType& b, Real dt)
     {
-        MomentumType eMomentum = e * particle.getCharge() * dt /
-            ((Real)2.0 * particle.getMass() * Constants<Real>::c());
-        MomentumType um = particle.getMomentum() / (particle.getMass() * Constants<Real>::c()) + eMomentum;
-        MomentumType t = b * particle.getCharge() * dt /
-            ((Real)2.0 * particle.getMass() * Constants<Real>::c() * sqrt((Real)1.0 + um.norm2()));
+        MomentumType eMomentum = e * particle->getCharge() * dt /
+            ((Real)2.0 * particle->getMass() * Constants<Real>::c());
+        MomentumType um = particle->getMomentum() / (particle->getMass() * Constants<Real>::c()) + eMomentum;
+        MomentumType t = b * particle->getCharge() * dt /
+            ((Real)2.0 * particle->getMass() * Constants<Real>::c() * sqrt((Real)1.0 + um.norm2()));
         MomentumType uprime = um + cross(um, t);
         MomentumType s = t * (Real)2.0 / ((Real)1.0 + t.norm2());
-        particle.setMomentum((um + cross(uprime, s) + eMomentum) * particle.getMass() * Constants<Real>::c());
-        PositionType position = particle.getPosition();
-        position += particle.getVelocity() * dt;
-        particle.setPosition(position);
+        particle->setMomentum((um + cross(uprime, s) + eMomentum) * particle->getMass() * Constants<Real>::c());
+        PositionType position = particle->getPosition();
+        position += particle->getVelocity() * dt;
+        particle->setPosition(position);
     }
 };
 
